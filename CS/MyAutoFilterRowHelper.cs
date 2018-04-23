ecf70149b9be5ee6e2b6fd5f09d05ef204927a07
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MyAutoFilterRowHelper.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.Utils.Drawing;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.Utils;
+
+namespace WindowsApplication1
+{
+    public class MyAutoFilterRowHelper
+    {
+        private readonly string _AutoFilterText;
+        private readonly GridView _View;
+        public MyAutoFilterRowHelper(GridView view, string text)
+        {
+            _View = view;
+            _AutoFilterText = text;
+            view.CustomDrawCell += view_CustomDrawCell;
+            view.GridControl.Paint += GridControl_Paint;
+        }
+
+
+        public Rectangle GetAutoFilterRowBounds()
+        {
+            GridViewInfo viewInfo = _View.GetViewInfo() as GridViewInfo;
+            GridDataRowInfo rowInfo = viewInfo.GetGridRowInfo(GridControl.AutoFilterRowHandle) as GridDataRowInfo;
+            if (rowInfo == null || (rowInfo.RowState & GridRowCellState.Focused) != 0)
+                return Rectangle.Empty;
+            Rectangle r = rowInfo.DataBounds;
+            if (r.X < viewInfo.ViewRects.ColumnPanelLeft) r.X = viewInfo.ViewRects.ColumnPanelLeft;
+            if (r.Right > viewInfo.ViewRects.Rows.Right) r.Width = viewInfo.ViewRects.Rows.Right - r.X;
+            return r;
+        }
+
+        void GridControl_Paint(object sender, PaintEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_View.ActiveFilterString))
+                return;
+            Rectangle bounds = GetAutoFilterRowBounds();
+            if (e.ClipRectangle.IntersectsWith(bounds))
+                DrawAutoFilterRowText(_View, new GraphicsCache(e.Graphics), bounds);
+        }
+
+        void view_CustomDrawCell(object sender, RowCellCustomDrawEventArgs e)
+        {
+            if (e.RowHandle == GridControl.NewItemRowHandle)
+                e.Handled = true;
+        }
+
+
+        private void DrawAutoFilterRowText(GridView view, GraphicsCache cache, Rectangle r)
+        {
+            AppearanceObject appearance = view.PaintAppearance.TopNewRow;
+            appearance.DrawBackground(cache, r);
+            appearance.DrawString(cache, _AutoFilterText, r);
+        }
+
+    }
+}
